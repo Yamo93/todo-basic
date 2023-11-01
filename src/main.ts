6f51beb8810b5ba707d1e348c18c6b4a691d4496
Kolegaaa <elias_king_9@hotmail.com>
2023-11-01 16:04:27 +0100

    update todo

--- src/main.ts
+++ src/main.ts
@@ -6,40 +6,46 @@ if (!app) throw new Error('App element is missing');
 
 const input = document.createElement('input');
 input.setAttribute('type', 'text');
-app.appendChild(input)
+app.appendChild(input);
 console.log(input);
 
 const button = document.createElement('button');
 button.setAttribute('type', 'button');
-button.textContent = 'add todo'
-app.appendChild(button)
+button.textContent = 'add todo';
+app.appendChild(button);
 console.log(button);
 
 const list = document.createElement('ul');
 app.appendChild(list);
 console.log(list);
 
+button.addEventListener('click', () => {
+    const li = document.createElement('li');
+    li.textContent = input.value;
+    list.appendChild(li);
+    const removeButton = document.createElement('button');
+    removeButton.setAttribute('type', 'button');
+    removeButton.textContent = 'Remove todo';
+    li.appendChild(removeButton);
+    const updateButton = document.createElement('button');
+    updateButton.setAttribute('type', 'button');
+    updateButton.textContent = 'Update todo';
+    li.appendChild(updateButton);
 
+    removeButton.addEventListener('click', () => {
+        if (confirm('Do you want to delete this task')) {
+            li.remove();
+        }
+    });
+    updateButton.addEventListener('click', () => {
+        const todoUpdate = prompt('Update your todo');
 
-button.addEventListener('click', () =>{
-  const li = document.createElement('li')
-  li.textContent = input.value
-  list.appendChild(li)
-  const removeButton = document.createElement('button')
-  removeButton.setAttribute('type', 'button')
-  removeButton.textContent = 'Remove todo'
-   li.appendChild(removeButton)
-    
-   removeButton.addEventListener('click',()=>{
-     if(confirm('Do you want to delete this task')){
-      li.remove()
-     }
-
-    
-      
-   })
-  
-})
-
-
-
+        if (todoUpdate === null || todoUpdate === '') {
+            alert('du måste skriva in något');
+        } else {
+          li.textContent = todoUpdate
+          li.appendChild(removeButton)
+          li.appendChild(updateButton)
+        }
+    });
+});
